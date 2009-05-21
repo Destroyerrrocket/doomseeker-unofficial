37c6db4b1da2eb7c261ce23c6f26293506ade839
Braden Obrzut <admin@maniacsvault.net>
2009-05-21 22:31:43 +0000

    - Bumped version to 0.2 Alpha - Converted Wadseeker to the LGPL - Removed some header files that contained nothing of use from the wadseeker CMakeLists.txt file.

--- src/wadseeker/wadseeker.cpp
+++ src/wadseeker/wadseeker.cpp
@@ -2,20 +2,20 @@
 // wadseeker.cpp
 //------------------------------------------------------------------------------
 //
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License
-// as published by the Free Software Foundation; either version 2
-// of the License, or (at your option) any later version.
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
 //
-// This program is distributed in the hope that it will be useful,
+// This library is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
 //
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-// 02110-1301, USA.
+// 02110-1301  USA
 //
 //------------------------------------------------------------------------------
 // Copyright (C) 2009 "Zalewa" <zalewapl@gmail.com>
@@ -94,7 +94,7 @@ QStringList Wadseeker::defaultSitesListEncoded()
 
 const QString Wadseeker::description()
 {
-	return "This program is distributed under the terms of the GPL v2.";
+	return "This library is distributed under the terms of the LGPL v2.1.";
 }
 
 void Wadseeker::downloadProgressSlot(int done, int total)
