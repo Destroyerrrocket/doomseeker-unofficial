aa119e85e2792ab5639e11e773d605eba6d7c7bc
Braden Obrzut <admin@maniacsvault.net>
2009-12-07 21:48:16 +0000

    - Server console (and thus RCon) now stores the last 50 commands into memory which can be accessed by pressing up and down.

---
+++ src/gui/widgets/serverconsole.cpp
@@ -0,0 +1,90 @@
+//------------------------------------------------------------------------------
+// serverconsole.cpp
+//------------------------------------------------------------------------------
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+// 02110-1301, USA.
+//
+//------------------------------------------------------------------------------
+// Copyright (C) 2009 "Blzut3" <admin@maniacsvault.net>
+//------------------------------------------------------------------------------
+
+#include "serverconsole.h"
+
+MemoryLineEdit::MemoryLineEdit(QWidget *parent) : QLineEdit(parent)
+{
+	connect(this, SIGNAL(returnPressed()), this, SLOT(storeCommand()));
+}
+
+void MemoryLineEdit::keyPressEvent(QKeyEvent *event)
+{
+	if(history.count() != 0)
+	{
+		if(event->key() == Qt::Key_Up)
+		{
+			if(position != history.begin())
+				position--;
+			setText(*position);
+		}
+		else if(event->key() == Qt::Key_Down && position != history.end())
+		{
+			position++;
+			if(position != history.end())
+				setText(*position);
+			else
+				setText("");
+		}
+	}
+	QLineEdit::keyPressEvent(event);
+}
+
+void MemoryLineEdit::storeCommand()
+{
+	history.append(text());
+	if(history.count() > 50)
+		history.removeFirst();
+	position = history.end();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+ServerConsole::ServerConsole(QWidget *parent, Qt::WindowFlags f) : QWidget(parent, f)
+{
+	setupUi(this);
+
+	consoleInput = new MemoryLineEdit();
+	layout()->addWidget(consoleInput);
+
+	connect(consoleInput, SIGNAL(returnPressed()), this, SLOT(forwardMessage()));
+}
+
+void ServerConsole::appendMessage(const QString &message)
+{
+	QString appendMessage = message;
+	if(appendMessage.endsWith('\n')) // Remove the trailing new line since appendPlainText seems to add one automatically.
+		appendMessage.chop(1);
+
+	consoleOutput->appendPlainText(appendMessage);
+}
+
+void ServerConsole::forwardMessage()
+{
+	QString msg = consoleInput->text();
+	if(msg[0] == ':')
+		msg.replace(0, 1, "say ");
+
+	emit messageSent(msg);
+	consoleInput->setText("");
+}
--- src/gui/widgets/serverconsole.h
+++ src/gui/widgets/serverconsole.h
@@ -1,39 +1,50 @@
 #ifndef SERVERCONSOLE_H
 #define SERVERCONSOLE_H
 
+#include <QLineEdit>
+#include <QList>
+
 #include "ui_serverconsole.h"
+
+/**
+ * Stores the input into a history that can be accessed by pressing up and down.
+ */
+class MemoryLineEdit : public QLineEdit
+{
+	Q_OBJECT
+
+	public:
+		MemoryLineEdit(QWidget *parent=0);
+
+	protected:
+		void	keyPressEvent(QKeyEvent *event);
+
+	protected slots:
+		void	storeCommand();
+
+	private:
+		QList<QString>				history;
+		QList<QString>::iterator	position;
+};
 
 class ServerConsole : public QWidget, private Ui::ServerConsole
 {
 	Q_OBJECT
 
 	public:
-		ServerConsole(QWidget *parent=NULL, Qt::WindowFlags f=0) : QWidget(parent, f)
-		{
-			setupUi(this);
-
-			connect(consoleInput, SIGNAL(returnPressed()), this, SLOT(forwardMessage()));
-		}
+		ServerConsole(QWidget *parent=NULL, Qt::WindowFlags f=0);
 
 	public slots:
-		void	appendMessage(const QString &message)
-		{
-			QString appendMessage = message;
-			if(appendMessage.endsWith('\n')) // Remove the trailing new line since appendPlainText seems to add one automatically.
-				appendMessage.chop(1);
-
-			consoleOutput->appendPlainText(appendMessage);
-		}
+		void	appendMessage(const QString &message);
 
 	signals:
 		void	messageSent(const QString &message);
 
 	protected slots:
-		void	forwardMessage()
-		{
-			emit messageSent(consoleInput->text());
-			consoleInput->setText("");
-		}
+		void	forwardMessage();
+
+	private:
+		MemoryLineEdit	*consoleInput;
 };
 
 #endif // SERVERCONSOLE_H
