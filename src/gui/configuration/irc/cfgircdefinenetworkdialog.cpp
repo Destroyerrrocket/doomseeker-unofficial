a905f9cd909ac61237cb0a4df4e4ddad7bc6d8b4
Zalewa <zalewapl@gmail.com>
2010-09-23 19:12:13 +0000

    - IRC: Began working on configuration UI.

---
+++ src/gui/configuration/irc/cfgircdefinenetworkdialog.cpp
@@ -0,0 +1,72 @@
+//------------------------------------------------------------------------------
+// cfgircdefinenetworkdialog.cpp
+//------------------------------------------------------------------------------
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+// 02110-1301, USA.
+//
+//------------------------------------------------------------------------------
+// Copyright (C) 2010 "Zalewa" <zalewapl@gmail.com>
+//------------------------------------------------------------------------------
+#include "cfgircdefinenetworkdialog.h"
+#include "irc/ircnetworkentity.h"
+
+CFGIRCDefineNetworkDialog::CFGIRCDefineNetworkDialog(const IRCNetworkEntity& initValuesEntity, QWidget* parent)
+: QDialog(parent)
+{
+	initFrom(initValuesEntity);
+}
+
+CFGIRCDefineNetworkDialog::CFGIRCDefineNetworkDialog(QWidget* parent)
+: QDialog(parent)
+{
+}
+
+void CFGIRCDefineNetworkDialog::buttonClicked(QAbstractButton* button)
+{
+	QPushButton* pButton = (QPushButton*)button;
+	if (pButton == buttonBox->button(QDialogButtonBox::Ok))
+	{
+		this->accept();
+	}
+	else
+	{
+		this->reject();
+	}	
+}
+		
+IRCNetworkEntity CFGIRCDefineNetworkDialog::getNetworkEntity() const
+{
+	IRCNetworkEntity entity;
+	
+	entity.address = this->leAddress->text();
+	entity.description = this->leDescription->text();
+	entity.nickservCommand = this->leNickservCommand->text();
+	entity.nickservPassword = this->leNickservPassword->text();
+	entity.password = this->leServerPassword->text();
+	entity.port = this->spinPort->value();
+	
+	return entity;
+}
+
+void CFGIRCDefineNetworkDialog::initFrom(const IRCNetworkEntity& networkEntity)
+{
+	this->leAddress->setText(networkEntity.address);
+	this->leDescription->setText(networkEntity.description);
+	this->leNickservCommand->setText(networkEntity.nickservCommand);
+	this->leNickservPassword->setText(networkEntity.nickservPassword);
+	this->leServerPassword->setText(networkEntity.password);
+	this->spinPort->setValue(networkEntity.port);
+}
---
+++ src/gui/configuration/irc/cfgircdefinenetworkdialog.h
@@ -0,0 +1,48 @@
+//------------------------------------------------------------------------------
+// cfgircdefinenetworkdialog.h
+//------------------------------------------------------------------------------
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+// 02110-1301, USA.
+//
+//------------------------------------------------------------------------------
+// Copyright (C) 2010 "Zalewa" <zalewapl@gmail.com>
+//------------------------------------------------------------------------------
+#ifndef __CFGIRCDEFINENETWORKDIALOG_H__
+#define __CFGIRCDEFINENETWORKDIALOG_H__
+
+#include "ui_cfgircdefinenetworkdialog.h"
+
+class IRCNetworkEntity;
+
+class CFGIRCDefineNetworkDialog : public QDialog, private Ui::CFGIRCDefineNetworkDialog
+{
+	Q_OBJECT
+	
+	public:
+		CFGIRCDefineNetworkDialog(const IRCNetworkEntity& initValuesEntity, QWidget* parent = NULL);
+		CFGIRCDefineNetworkDialog(QWidget* parent = NULL);
+		
+		IRCNetworkEntity				getNetworkEntity() const;
+		
+	private:
+		void							initFrom(const IRCNetworkEntity& networkEntity);
+		
+	private slots:
+		void							buttonClicked(QAbstractButton* button);
+	
+};
+
+#endif
