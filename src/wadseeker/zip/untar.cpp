185ba6b383f3b71da300c6c8ec4302e2dbe6a4d3
Braden Obrzut <admin@maniacsvault.net>
2011-04-17 16:16:07 +0000

    - Forgot to actually commit the untar files.

---
+++ src/wadseeker/zip/untar.cpp
@@ -0,0 +1,108 @@
+//------------------------------------------------------------------------------
+// untar.cpp
+//------------------------------------------------------------------------------
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+// 02110-1301  USA
+//
+//------------------------------------------------------------------------------
+// Copyright (C) 2011 "Blzut3" <admin@maniacsvault.net>
+//------------------------------------------------------------------------------
+
+#include "untar.h"
+
+#include <QBuffer>
+#include <QFile>
+
+UnTar::UnTar(const QByteArray &data) : valid(true), data(data)
+{
+	stream = new QBuffer(&this->data);
+	scanTarFile();
+}
+
+UnTar::UnTar(const QString &filename) : valid(true)
+{
+	stream = new QFile(filename);
+	scanTarFile();
+}
+
+UnTar::~UnTar()
+{
+	delete stream;
+}
+
+bool UnTar::extract(int file, const QString &where)
+{
+	stream->open(QIODevice::ReadOnly);
+	stream->seek(directory[file].offset);
+	QByteArray fileData = stream->read(directory[file].size);
+	stream->close();
+
+	if(fileData.size() == directory[file].size)
+	{
+		QFile outputFile(where);
+		outputFile.open(QFile::WriteOnly);
+		outputFile.write(fileData);
+		outputFile.close();
+		return true;
+	}
+	return false;
+}
+
+QString UnTar::fileNameFromIndex(int file)
+{
+	if(file >= directory.size())
+		return QString();
+	return directory[file].filename;
+}
+
+int UnTar::findFileEntry(const QString &entryName)
+{
+	for(int i = 0;i < directory.count();i++)
+	{
+		if(directory[i].filename == entryName)
+			return i;
+	}
+	return -1;
+}
+
+void UnTar::scanTarFile()
+{
+	TarFile file;
+	char buffer[512];
+	qint64 read;
+	unsigned int offset = 512;
+
+	stream->open(QIODevice::ReadOnly);
+	while(valid && (read = stream->read(buffer, 512)) > 0)
+	{
+		if(read < 512)
+		{
+			valid = false;
+			break;
+		}
+
+		file.filename = QString::fromAscii(buffer, qMin(static_cast<int>(strlen(buffer)), 100));
+		if(file.filename.isEmpty())
+			break;
+		file.size = QString::fromAscii(&buffer[124], 12).toUInt(&valid, 8);
+		file.offset = offset;
+		offset += 512 + file.size;
+		stream->seek(offset-512); 
+
+		directory.append(file);
+	}
+	stream->close();
+}
---
+++ src/wadseeker/zip/untar.h
@@ -0,0 +1,64 @@
+//------------------------------------------------------------------------------
+// untar.h
+//------------------------------------------------------------------------------
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+// 02110-1301  USA
+//
+//------------------------------------------------------------------------------
+// Copyright (C) 2011 "Blzut3" <admin@maniacsvault.net>
+//------------------------------------------------------------------------------
+
+#ifndef __UNTAR_H__
+#define __UNTAR_H__
+
+#include <QByteArray>
+#include <QIODevice>
+#include <QList>
+#include <QObject>
+
+#include "unarchive.h"
+
+class UnTar : public UnArchive
+{
+	Q_OBJECT
+
+	public:
+		UnTar(const QByteArray &data);
+		UnTar(const QString &filename);
+		~UnTar();
+
+		bool	extract(int file, const QString &where);
+		QString	fileNameFromIndex(int file);
+		int		findFileEntry(const QString &entryName);
+		bool	isValid() { return valid; }
+
+	protected:
+		struct TarFile
+		{
+			QString			filename;
+			unsigned int	size;
+			unsigned int	offset;
+		};
+
+		void	scanTarFile();
+
+		bool			valid;
+		QList<TarFile>	directory;
+		QByteArray		data;
+		QIODevice		*stream;
+};
+
+#endif
