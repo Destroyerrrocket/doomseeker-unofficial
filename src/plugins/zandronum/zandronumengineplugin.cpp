7405fa4296bc754e67320fd63b75ef954be5b7e8
Braden Obrzut <admin@maniacsvault.net>
2013-07-06 02:36:12 -0400

    - Bumped Zandronum plugin version, so it can be distributed through the auto updater.

--- src/plugins/zandronum/zandronumengineplugin.cpp
+++ src/plugins/zandronum/zandronumengineplugin.cpp
@@ -48,7 +48,7 @@ ZandronumEnginePlugin::ZandronumEnginePlugin()
 
 	init("Zandronum", zandronum_xpm,
 		EP_Author, "The Doomseeker Team",
-		EP_Version, 2,
+		EP_Version, 3,
 
 		EP_AllowsURL,
 		EP_AllowsEmail,
