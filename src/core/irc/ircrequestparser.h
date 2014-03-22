50881e7cf269b180c08585293fbf1b2531672232
Zalewa <zalewapl@gmail.com>
2014-03-22 01:02:48 +0100

    - Formatting fix for IRCRequestParser doc.

--- src/core/irc/ircrequestparser.h
+++ src/core/irc/ircrequestparser.h
@@ -29,23 +29,23 @@
 class IRCAdapterBase;
 
 /**
- *	@brief Parses request and interprets them in a way that emulates
- *	mIRC (or any even slightly sane IRC client for that matter).
+ * @brief Parses request and interprets them in a way that emulates
+ *        mIRC (or any even slightly sane IRC client for that matter).
  *
- *	The above statement means that a non-RFC compliant request form that users 
- *	grew accustomed to will be understood correctly by this parser and
- *	converted to RFC 1459 compliant form.
+ * The above statement means that a non-RFC compliant request form that users
+ * grew accustomed to will be understood correctly by this parser and
+ * converted to RFC 1459 compliant form.
  *
- *	For example:
- *	- If RFC command begins with '/' character this character will be always 
- *	  trimmed. If command doesn't begin with '/' character error will be 
- *	  returned.
- *	  @b Example: "/who <nickname>" will be converted to "who <nickname>"
- *	- ':' characters will be inserted into some commands.
- *	  @b Example: "/part #channel I am leaving!" to 
- *	  "part #channel :I am leaving!"
- *	
- *	For more information refer to parse() method.
+ * For example:
+ * - If RFC command begins with '/' character this character will be always
+ *   trimmed. If command doesn't begin with '/' character error will be
+ *   returned.
+ *   @b Example: "/who <nickname>" will be converted to "who <nickname>"
+ * - ':' characters will be inserted into some commands.
+ *   @b Example: "/part #channel I am leaving!" to
+ *   "part #channel :I am leaving!"
+ * 
+ * For more information refer to parse() method.
  */
 class IRCRequestParser : public QObject
 {
@@ -69,7 +69,7 @@ class IRCRequestParser : public QObject
 
 		/**
 		 * @brief Parses input string and returns it through output string.
-		 * Additional information is passed through return value.
+		 *        Additional information is passed through return value.
 		 *
 		 * @param pAdapter
 		 *     Adapter that sends the message.
@@ -95,24 +95,24 @@ class IRCRequestParser : public QObject
 
 	signals:
 		/**
-		 *	@brief Echoes back all PRIVMSG commands.
-		 *	
-		 *	@param recipient
-		 *		Recipient of the message. This is extracted
-		 *		directly from the privmsg request.
-		 *	@param messageContent
-		 *		Content of the message.
+		 * @brief Echoes back all PRIVMSG commands.
+		 * 
+		 * @param recipient
+		 *     Recipient of the message. This is extracted
+		 *     directly from the privmsg request.
+		 * @param messageContent
+		 *     Content of the message.
 		 */
 		void echoPrivmsg(const QString& recipient, const QString& content);
 		
 		/**
-		 *	@brief Emitted when "/query" alias is used.
+		 * @brief Emitted when "/query" alias is used.
 		 *
-		 *	This signal will only be emitted if the query
-		 *	parameter specifies a valid user name.
+		 * This signal will only be emitted if the query
+		 * parameter specifies a valid user name.
 		 *
-		 *	@param who
-		 *		A clean nickname.
+		 * @param who
+		 *     A clean nickname.
 		 */
 		void query(const QString& who);
 
