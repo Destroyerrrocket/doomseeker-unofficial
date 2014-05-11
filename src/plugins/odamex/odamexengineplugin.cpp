63f6f80e1f403853af3f445d16d5fa2bbeac7425
Zalewa <zalewapl@gmail.com>
2014-05-11 18:43:39 +0200

    - Bumped versions of everything.

--- src/plugins/odamex/odamexengineplugin.cpp
+++ src/plugins/odamex/odamexengineplugin.cpp
@@ -41,7 +41,7 @@ OdamexEnginePlugin::OdamexEnginePlugin()
 
 	init("Odamex", odamex_xpm,
 		EP_Author, "The Doomseeker Team",
-		EP_Version, 17,
+		EP_Version, 18,
 
 		EP_AllowsURL,
 		EP_AllowsEmail,
