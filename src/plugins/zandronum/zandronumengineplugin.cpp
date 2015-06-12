0ce2602aef5b32f03b08e1134e1a0255d6a1cab5
Zalewa <zalewapl@gmail.com>
2015-06-12 23:27:53 +0200

    - Bumped plugins versions for release in beta update channel.

--- src/plugins/zandronum/zandronumengineplugin.cpp
+++ src/plugins/zandronum/zandronumengineplugin.cpp
@@ -58,7 +58,7 @@ ZandronumEnginePlugin::ZandronumEnginePlugin()
 
 	init("Zandronum", zandronum_xpm,
 		EP_Author, "The Doomseeker Team",
-		EP_Version, 23,
+		EP_Version, 24,
 
 		EP_AllowsURL,
 		EP_AllowsEmail,
