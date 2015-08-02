f8be0048f189972187cc7aef3d988b3a2748b78f
Braden Obrzut <admin@maniacsvault.net>
2015-08-02 03:46:52 -0400

    - Fixed: Wrong package name on doomseeker-core prevented Qt5 migration on Mac. - Fixed: Package file listing error preventing QtWidgets from being installed on Mac. - Forgot to commit the new package scripts before.

---
+++ tools/updateinstaller/tools/macosx-configs/doomseeker-core.js
@@ -0,0 +1,15 @@
+{
+    "name" : "doomseeker-core",
+    "files" : [
+        "Contents/MacOS/translations/translations.def",
+        "Contents/MacOS/translations/pl_PL.qm",
+        "Contents/MacOS/translations/qt_pl.qm",
+        "Contents/MacOS/doomseeker",
+        "Contents/MacOS/updater",
+        "Contents/Info.plist",
+        "Contents/Resources/icon-osx.icns"
+    ],
+
+    "updater-binary" : "Contents/MacOS/updater",
+    "main-binary" : "Contents/MacOS/doomseeker"
+}
