2d55d1d8f80fc985d20ab46f1aad03b04308a71f
Zalewa <zalewapl@gmail.com>
2016-01-17 18:20:22 +0100

    - SRB2: Auto-update package config for SRB2 plugin.

---
+++ tools/updateinstaller/tools/win32-configs/p-sonicroboblast2.js
@@ -0,0 +1,9 @@
+{
+    "name" : "p-sonicroboblast2",
+    "files" : [
+        "engines/libsrb2.dll",
+        "translations/srb2_pl_PL.qm"
+    ],
+    "updater-binary" : "updater.exe",
+    "main-binary" : "doomseeker.exe"
+}
