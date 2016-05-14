e38679b70f8e9b4304af35c9b9193ff6888e49b8
Zalewa <zalewapl@gmail.com>
2016-05-14 13:03:05 +0200

    - geolite2_conv.py script improvements: -- Script can now automatically download GeoLite2 database from source URL to temp space. Database is then unzipped and converted to Doomseeker's format. -- License added. -- Script's main working code wrapped in run() function to avoid variable bleeding into other functions.

--- tools/geolite2_conv.py
+++ tools/geolite2_conv.py
@@ -1,12 +1,38 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
+#
+# GeoLite2 conversion script (c) The Doomseeker Team 2016.
+#
+# This script converts GeoLite2 database from the original CSV format into
+# compacted format known to Doomseeker. Compacted format is optimized for faster
+# loading and for searching country by IP.
+#
+# Database can either be already stored in locally accessible filesystem or the
+# script can automatically download the newest version directly from MaxMind
+# website. Run with '-h' argument for usage details.
+#
+# ----------------------------------------
+#
+# GeoLite2 - Database and Contents Copyright (c) 2016 MaxMind, Inc.
+#
+# GeoLite2 is licensed under the Creative Commons Attribution-ShareAlike 3.0
+# Unported License. To view a copy of this license, visit
+# http://creativecommons.org/licenses/by-sa/3.0/.
+#
 import csv
 import json
 import os
 import sys
+import shutil
 import struct
+import tempfile
+import urllib
+import zipfile
 from copy import copy
 from multiprocessing import Pool
+
+
+country_mapping = None
 
 
 class Network(object):
@@ -68,6 +94,72 @@ class Location(object):
         return "{0},{1}".format(self.name, self.code)
 
 
+def download_db():
+    url = "http://geolite.maxmind.com/download/geoip/database/GeoLite2-Country-CSV.zip"
+    f = None
+    try:
+        with tempfile.NamedTemporaryFile(delete=False) as f:
+            download_url(url, f)
+        tmpdir = tempfile.mkdtemp()
+        unzip(f.name, tmpdir)
+    finally:
+        if f:
+            try:
+                os.unlink(f.name)
+            except Exception as e:
+                print >>sys.stderr, "Warning: failed to delete file {0}:\n\t{1}".format(f.name, e)
+    return find_geolite_files(tmpdir), tmpdir
+
+
+def download_url(url, outfile):
+    stream = urllib.urlopen(url)
+    try:
+        print >>sys.stderr, "Receiving URL {0}".format(url)
+        collected = 0
+        while True:
+            chunk = stream.read(1024 * 20)
+            if not chunk:
+                break
+            collected += len(chunk)
+            print >>sys.stderr, "Downloaded {0} B".format(collected)
+            outfile.write(chunk)
+    finally:
+        stream.close()
+
+
+def unzip(zippath, outdir):
+    print >>sys.stderr, "Extracting downloaded file '{0}'\n\tto directory '{1}'".format(
+        zippath, outdir)
+    with zipfile.ZipFile(zippath) as zf:
+        zf.extractall(outdir)
+
+
+def find_geolite_files(maindir):
+    # GeoLite2 zip contains 1 entry in its root which is a directory with a
+    # changing name that contains release date.
+    for entry in os.listdir(maindir):
+        geolite_dir = os.path.join(maindir, entry)
+        break
+    geolite_blocks_path = os.path.join(geolite_dir, "GeoLite2-Country-Blocks-IPv4.csv")
+    geolite_countries_path = os.path.join(geolite_dir, "GeoLite2-Country-Locations-en.csv")
+    paths = (geolite_blocks_path, geolite_countries_path)
+    for f in paths:
+        if not os.path.isfile(f):
+            raise IOError("File '{0}' not found".format(f))
+    return paths
+
+
+def reduce_block(block):
+    geoid, networks = block
+    sys.stderr.write("geoid {0}, networks {1}\n".format(geoid, len(networks)))
+    sys.stderr.flush()
+    ipranges = reduce_networks(networks)
+    sys.stderr.write("geoid {0} - DONE, networks reduced from {1} to {2}\n".format(
+        geoid, len(networks), len(ipranges)))
+    sys.stderr.flush()
+    return geoid, ipranges
+
+
 def reduce_networks(networks):
     ipranges = networks_to_ipranges(networks)
     ipranges = merge_consecutive_ipranges(ipranges)
@@ -105,7 +197,7 @@ def merge_inclusive_ipranges(ipranges):
             if iprange[0] > merged_range[0] and iprange[1] < merged_range[1]:
                 skip = True
                 break
-            elif iprange[0] < merged_range[0] and iprange1 > merged_range[1]:
+            elif iprange[0] < merged_range[0] and iprange[1] > merged_range[1]:
                 merged_range[0] = iprange[0]
                 merged_range[1] = iprange[1]
                 skip = True
@@ -123,80 +215,103 @@ def merge_inclusive_ipranges(ipranges):
     return merged
 
 
-if len(sys.argv) < 4:
+def is_help():
+    return "-h" in sys.argv or "--help" in sys.argv
+
+
+def usage():
     print >>sys.stderr, "Usage: {0} <geolite2_blocks.csv> <geolite2_locations.csv> <doomseeker.db>".format(sys.argv[0])
-    exit(2)
+    print >>sys.stderr, "   or: {0} <doomseeker.db>  # Auto-downloads GeoLite2 database".format(sys.argv[0])
+    print >>sys.stderr, "   or: {0} <-h|--help>      # Prints help".format(sys.argv[0])
 
 
-scriptdir = os.path.dirname(os.path.realpath(__file__))
-with open(os.path.join(scriptdir, "country_iso3.json"), "rb") as f:
-    country_mapping = json.loads(f.read())
+def convert(geolite_blocks_path, geolite_locations_path, doomseeker_path):
+    with open(geolite_blocks_path, "rb") as f:
+        reader = csv.reader(f)
+        blocks = {}
+        for rownum, row in enumerate(reader):
+            if rownum == 0:
+                # Header row.
+                continue
+            network = Network(row[0])
+            geoid = row[1] if row[1] else row[2]
+            if not geoid:
+                continue
+            geoid = int(geoid)
+            if geoid in blocks:
+                blocks[geoid].append(network)
+            else:
+                blocks[geoid] = [network]
 
 
-geolite_path = sys.argv[1]
-with open(geolite_path, "rb") as f:
-    reader = csv.reader(f)
-    blocks = {}
-    for rownum, row in enumerate(reader):
-        if rownum == 0:
-            # Header row.
-            continue
-        network = Network(row[0])
-        geoid = row[1] if row[1] else row[2]
-        if not geoid:
-            continue
-        geoid = int(geoid)
-        if geoid in blocks:
-            blocks[geoid].append(network)
+    with open(geolite_locations_path, "rb") as f:
+        reader = csv.reader(f)
+        locations = {}
+        for rownum, row in enumerate(reader):
+            if rownum == 0:
+                # Header row
+                continue
+            geoid = int(row[0])
+            location = Location(geoid=geoid, continent_code=row[2],
+                                continent_name=row[3],
+                                country_code=row[4], country_name=row[5])
+            locations[geoid] = location
+
+
+    pool = Pool()
+    try:
+        items = list(blocks.iteritems())
+        items.sort(key=lambda el: len(el[1]), reverse=True)
+        blocks = dict(pool.imap_unordered(reduce_block, items))
+    finally:
+        pool.terminate()
+        pool.close()
+
+
+    with open(doomseeker_path, "wb") as f:
+        f.write("IP2C")
+        f.write(struct.pack("<H", 2))  # Version
+        num = 1
+        for geoid, location in locations.iteritems():
+            f.write("{0}\0{1}\0".format(location.name, location.code))
+            f.write(struct.pack("<I", len(blocks[geoid])))
+            for network in blocks[geoid]:
+                f.write(struct.pack("<II", network[0], network[1]))
+            num += 1
+
+
+def run():
+    if is_help():
+        usage()
+        exit(0)
+
+    scriptdir = os.path.dirname(os.path.realpath(__file__))
+    with open(os.path.join(scriptdir, "country_iso3.json"), "rb") as f:
+        global country_mapping
+        country_mapping = json.loads(f.read())
+
+    tmpdir = None
+    try:
+        if len(sys.argv) == 2:
+            geolite_files, tmpdir = download_db()
+            geolite_blocks_path, geolite_locations_path = geolite_files
+            doomseeker_path = sys.argv[1]
+        elif len(sys.argv) == 4:
+            geolite_blocks_path = sys.argv[1]
+            geolite_locations_path = sys.argv[2]
+            doomseeker_path = sys.argv[3]
         else:
-            blocks[geoid] = [network]
+            usage()
+            exit(2)
+
+        convert(geolite_blocks_path, geolite_locations_path, doomseeker_path)
+    finally:
+        if tmpdir:
+            try:
+                shutil.rmtree(tmpdir)
+            except Exception as e:
+                print >>sys.stderr, "Warning: failed to clean up tmpdir '{0}': {1}".format(tmpdir, e)
 
 
-geolite_locations_path = sys.argv[2]
-with open(geolite_locations_path, "rb") as f:
-    reader = csv.reader(f)
-    locations = {}
-    for rownum, row in enumerate(reader):
-        if rownum == 0:
-            # Header row
-            continue
-        geoid = int(row[0])
-        location = Location(geoid=geoid, continent_code=row[2],
-                            continent_name=row[3],
-                            country_code=row[4], country_name=row[5])
-        locations[geoid] = location
-
-
-def reduce_block(block):
-    geoid, networks = block
-    len_before = len(networks)
-    sys.stderr.write("geoid {0}, networks {1}\n".format(geoid, len(networks)))
-    sys.stderr.flush()
-    ipranges = reduce_networks(networks)
-    sys.stderr.write("geoid {0} - DONE, networks reduced from {1} to {2}\n".format(
-        geoid, len(networks), len(ipranges)))
-    sys.stderr.flush()
-    return geoid, ipranges
-
-
-pool = Pool()
-try:
-    items = list(blocks.iteritems())
-    items.sort(key=lambda el: len(el[1]), reverse=True)
-    blocks = dict(pool.imap_unordered(reduce_block, items))
-finally:
-    pool.terminate()
-    pool.close()
-
-
-doomseeker_path = sys.argv[3]
-with open(doomseeker_path, "wb") as f:
-    f.write("IP2C")
-    f.write(struct.pack("<H", 2))  # Version
-    num = 1
-    for geoid, location in locations.iteritems():
-        f.write("{0}\0{1}\0".format(location.name, location.code))
-        f.write(struct.pack("<I", len(blocks[geoid])))
-        for network in blocks[geoid]:
-            f.write(struct.pack("<II", network[0], network[1]))
-        num += 1
+if __name__ == "__main__":
+    run()
