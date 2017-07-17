f7d560d5ec8df150b84d6eb0df182428c47ea334
Zalewa <zalewapl@gmail.com>
2017-07-17 09:50:22 +0200

    - Turok2Ex: Added auto-updater configurations.

---
+++ tools/updateinstaller/tools/macosx-configs/p-turok2remaster.js
@@ -0,0 +1,9 @@
+{
+    "name" : "p-turok2remaster",
+    "files" : [
+        "Contents/MacOS/engines/libturok2ex.so",
+        "Contents/MacOS/translations/turok2ex_pl_PL.qm"
+    ],
+    "updater-binary" : "Contents/MacOS/updater",
+    "main-binary" : "Contents/MacOS/doomseeker"
+}
---
+++ tools/updateinstaller/tools/win32-configs/p-turok2remaster.js
@@ -0,0 +1,9 @@
+{
+    "name" : "p-turok2remaster",
+    "files" : [
+        "engines/libturok2ex.dll",
+        "translations/turok2ex_pl_PL.qm"
+    ],
+    "updater-binary" : "updater.exe",
+    "main-binary" : "doomseeker.exe"
+}
