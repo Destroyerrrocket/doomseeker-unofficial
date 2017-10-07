0584afe23f655bb6f418ed8bad93dd3d4e7f9f81
Zalewa <zalewapl@gmail.com>
2017-10-07 13:51:52 +0200

    - Added missing copyright notices & preambles to releasescripts/*, build-update-kit.rb and doxygen.dox.

--- releasescripts/makemacdmg.sh
+++ releasescripts/makemacdmg.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
+#------------------------------------------------------------------------------
+# makemacdmg.sh
+#------------------------------------------------------------------------------
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301  USA
+#
+#------------------------------------------------------------------------------
+# Copyright (C) 2010 Braden "Blzut3" Obrzut <admin@maniacsvault.net>
+#------------------------------------------------------------------------------
 
 SDK_10_4=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.4u.sdk
 SDK_10_9=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk
