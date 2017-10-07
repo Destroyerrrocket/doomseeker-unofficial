1427aad96d64db7b55701f9aff7d07758f2bba37
Zalewa <zalewapl@gmail.com>
2017-10-07 12:27:11 +0200

    - Fixed "The Doomseeker TEam" typo in Mendeley's create-version-info.bat

--- tools/updateinstaller/src/resources/create-version-info.bat
+++ tools/updateinstaller/src/resources/create-version-info.bat
@@ -1,7 +1,7 @@
 set VERSION="1.0"
 set FILEDESCR=/s desc "Software update utility."
 set BUILDINFO=/s pb "Built by The Doomseeker Team"
-set COMPINFO=/s company "The Doomseeker Team" /s (c) "(C) The Doomseeker TEam 2012"
+set COMPINFO=/s company "The Doomseeker Team" /s (c) "(C) The Doomseeker Team 2012"
 set PRODINFO=/s product "Doomseeker Updater" /pv "1.0"
 
 verpatch /vo /xi updater.exe %VERSION% %FILEDESCR% %COMPINFO% %PRODINFO% %BUILDINFO%
