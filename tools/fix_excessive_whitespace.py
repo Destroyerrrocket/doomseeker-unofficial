331a9819230494bd1d09202693dff8dfff8aeea4
Zalewa <zalewapl@gmail.com>
2018-02-22 11:16:05 +0100

    - Relicensed scripts in tools/ to LGPLv2.1+.

--- tools/fix_excessive_whitespace.py
+++ tools/fix_excessive_whitespace.py
@@ -4,20 +4,20 @@
 # fix_excessive_whitespace.py
 #-------------------------------------------------------------------------------
 #
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
 #
-# This program is distributed in the hope that it will be useful,
+# This library is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-# 02110-1301, USA.
+# 02110-1301  USA
 #
 #-------------------------------------------------------------------------------
 # Copyright (C) 2014 "Zalewa" <zalewapl@gmail.com>
@@ -90,14 +90,19 @@ def process_file(f):
 	return "".join(lines)
 
 
-rootdir = sys.argv[1]
-for root, dirs, files in os.walk(rootdir):
-	for fname in files:
-		if not fname.endswith(".h"):
-			continue
-		fpath = os.path.join(root, fname)
-		print >>sys.stderr, fpath
-		with open(os.path.join(root, fname), "rb") as f:
-			output = process_file(f)
-		with open(os.path.join(root, fname), "wb") as f:
-			f.write(output)
+def main():
+	rootdir = sys.argv[1]
+	for root, dirs, files in os.walk(rootdir):
+		for fname in files:
+			if not fname.endswith(".h"):
+				continue
+			fpath = os.path.join(root, fname)
+			print >>sys.stderr, fpath
+			with open(os.path.join(root, fname), "rb") as f:
+				output = process_file(f)
+			with open(os.path.join(root, fname), "wb") as f:
+				f.write(output)
+
+
+if __name__ == "__main__":
+	main()
--- tools/fixindentation.sh
+++ tools/fixindentation.sh
@@ -4,20 +4,20 @@
 # fixindentation.sh
 #-------------------------------------------------------------------------------
 #
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
 #
-# This program is distributed in the hope that it will be useful,
+# This library is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-# 02110-1301, USA.
+# 02110-1301  USA
 #
 #-------------------------------------------------------------------------------
 # Copyright (C) 2011 "Zalewa" <zalewapl@gmail.com>
@@ -25,10 +25,10 @@
 
 ###
 # Name:           fixindentation.sh
-# Description:    Converts all occurences of 4 spacebars into a single tab 
+# Description:    Converts all occurences of 4 spacebars into a single tab
 #                 character in every line of all *.h and *.cpp files in current
-#                 working directory and its sub-directories. 
-#                 This only applies to spacebars found at the beginning of each 
+#                 working directory and its sub-directories.
+#                 This only applies to spacebars found at the beginning of each
 #                 line.
 ##
 
